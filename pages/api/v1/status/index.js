8ce9f848590784864abc1529a8fb7450261f4ceb
gessecarneiro <55324790+gessecarneiro@users.noreply.github.com>
2023-11-16 19:44:36 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows)
   response.status(200).json({
-    "ola":"amigos",
-  })
+    ola: "amigos",
+  });
 }
 
 export default status;
