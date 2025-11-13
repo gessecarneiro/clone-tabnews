bde71e73bb9ead69bec26a69b312c265dcb0d786
gessecarneiro <55324790+gessecarneiro@users.noreply.github.com>
2025-11-13 03:41:22 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,8 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
-  console.log("Credenciais do postgres: ",
-    {
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-    }
-  )
   
   try {
     await client.connect();
